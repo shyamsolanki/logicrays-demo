6e47917884676d9c3704c3ee785238eccf2eafa5
shyamsolanki <er.shyamsolanki@gmail.com>
2021-08-08 21:39:22 +0530

    my javascript data

---
+++ Shyam data/Javascript/JsTypes_external.js
@@ -0,0 +1,6 @@
+            document.write("<h1>Personal Details</h1>")            
+            document.write("My Name IS Shyam Solanki");
+            document.write("<br>Address:B303 Somya residency Nr-Ujala Circle Sarkhej Ahemdabad");
+            document.write("<br>Class:Logicrays Acadamy Ahemdabad");
+            document.write("<br>Hobby:Music,Cricket");
+            document.write("<br>Langauge:Gujrati,Hindi,English");
---
+++ Shyam data/Javascript/PersonalData.js
@@ -0,0 +1,17 @@
+//Js Variable Declaration
+var frstNm,secNm,classNm,address;
+var mobNo;
+//Js Variable initialization
+frstNm="Shyam";
+secNm="Solanki";
+classNm="LogicRays Acadamy";
+address="B-303,Somya residency Nr-Ujala Circle Sarkhej Ahemdabad";
+mobNo="9574168567";
+
+//display all recored of personal details
+document.write("<center><h1><u>Personal Details</u><h1></center>");
+document.write("<center><br>First Name:"+frstNm);
+document.write("<center><br>Second Name:"+secNm);
+document.write("<center><br>Class Name:"+classNm);
+document.write("<center><br>address:"+address);
+document.write("<center><br>Mobile Number:"+mobNo);
